99c3f481aa5bedc1c7240fbbd8d35966c6ae0152
Nabeen-Thapa <nabeen2058@gmail.com>
2024-10-26 23:04:23 +0545

    add property on my express api add blog, view blog, delete blog after login

--- src/controllers/view_blog.js
+++ src/controllers/view_blog.js
@@ -10,6 +10,16 @@ viewBlog.get('/view-blog', async (req, res) => {
         return sendBadRequestError(res, "userEmail field is required");
     }
     try {
+        //check registered or not on user_details collection
+        const userdata = await collection.find({ email: userEmail });
+        if (userdata.length === 0) {
+            return sendInvalidRequestError(res, "User not registered yet. Please register first.");
+        }
+        //ckeck with user_TOken collction
+        const userTokenData = await collectionToken.find({ userEmail });
+        if (userTokenData.length === 0) {
+            return sendInvalidRequestError(res, "User not logged in. Please log in first.");
+        }
         // Find posts based on userEmail
         const userPosts = await collectionPost.find({ userEmail });
 
